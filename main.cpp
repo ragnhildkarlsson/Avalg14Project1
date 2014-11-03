9d393dfc4f711b26892b0676b25ce1e343af3b50
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2014-11-03 10:58:33 +0100

    added check for quick to factorize numbers

--- main.cpp
+++ main.cpp
@@ -13,14 +13,6 @@ using namespace std;
 static const  int primes[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
 static const int primeLength = 26;
 
-void printBigInt(){
-	mpz_class a;
-
-	a = "1284497587348578763896789346950";
-	cout <<  "a is " << a << "\n";
-
-}
-
 mpz_class pow(mpz_class base, unsigned long exp){
 		mpz_class res;
 		mpz_t temp;
@@ -53,7 +45,7 @@ mpz_class gcd(mpz_class n1, mpz_class n2){
 }
 
 
-mpz_class * genTestData(int size, int j){
+mpz_class * genTestData(int j){
 	
 	mpz_class sufix,ten, startval;
 	ten = 10;
@@ -65,13 +57,13 @@ mpz_class * genTestData(int size, int j){
 	startval = micke;
 	startval = startval*sufix;
 	int index = 0;
-	for(int i=1; i<= size;++i){
+	for(int i=1; i<= 100;++i){
 		data[index] = startval+i;
 		++index;
 	}
 	startval = ragnhild;
 	startval = startval*sufix;	
-	for(int i=1; i<= size;++i){
+	for(int i=1; i<= 100;++i){
 		data[index] = startval+i;
 		++index;
 	}
@@ -164,6 +156,10 @@ int isPrime(mpz_class n, int rep){
 mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 	// check small primes
 
+	std::clock_t pClock;
+    double duration;
+    pClock = std::clock();
+
 	for(int i = 0; i < primeLength; ++i){
 		if((N % primes[i]) == 0){
 			return primes[i];
@@ -177,23 +173,13 @@ mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 	b = seed;
 
 	int maxIter;
-	int maxInt;
-	maxIter = 1000;
+	//int maxInt;
+	maxIter = 100;
 
-	// //Large N
-	maxInt = std::numeric_limits<int>::max();
-	// maxIter = maxInt;
 
-	// //Medium N
-	// medium =10;
-	// medium = pow(medium,20);
-
-	// if(N<medium){
-	// 	maxIter = 100;	
-	// }
 	
 	//small N
-	if(N<maxInt){
+	if(N<10000000){
 		maxIter = 1;
 	}
 	
@@ -216,8 +202,14 @@ mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 		if(p>1){
 			return p;
 		}
+		duration = (std::clock() - pClock ) / (double) CLOCKS_PER_SEC;
+	}while(a!=b && duration<5);
 
-	}while(a!=b);
+
+	
+    cout<<"Calculation time: "<< duration <<'\n';	
+
+
 
 	p = "-1";
 	return p;
@@ -225,15 +217,14 @@ mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 }
 
 
-void factorize(mpz_class N, int trials){
-	cout << "\n Trying to factorize " << N;
+int factorize(mpz_class N, int trials){
+	cout << "\n Trying to factorize \n" << N <<"\n";
 	
 	std::clock_t start;
     double duration;
 
     start = std::clock();
 
-    cout << start;
 	vector<mpz_class> factors;	
 	gmp_randclass rand (gmp_randinit_default);
 	
@@ -249,18 +240,18 @@ void factorize(mpz_class N, int trials){
 		}
 
 		while(!primFound){
-			cerr << "\n Entering primFound loop with factor " << factor;
+			//cerr << "\n Entering primFound loop with factor " << factor;
 			for(int i = 0; i < trials; ++i){
 				seed = rand.get_z_range(factor-1) +1;
 				add = 1; //TODO Hard CODED
-				cerr << "\n Seed is " << seed << " add is " << add;
+				//cerr << "\n Seed is " << seed << " add is " << add;
 				temp = pollard(seed, add, factor);
 					if(temp > 0){
 						factor = temp;
 						//cerr << "\nIs the factor " << factor << " considered prime? " << isPrime(factor,10);
 						if(factor > 0 && isPrime(factor,10)){
 							factors.push_back(factor);
-							cerr << "\n Added factor " << factor;
+							//cerr << "\n Added factor " << factor;
 							primFound = true;
 							break;
 						}
@@ -273,7 +264,7 @@ void factorize(mpz_class N, int trials){
 				// Tried trial times and still failed?
 				if(temp < 0){
 					cout << "\n Factorization failed!";
-					return;
+					return 0;
 				}
 
 			}
@@ -289,7 +280,9 @@ void factorize(mpz_class N, int trials){
 	}
 	cout << "\n";
 	duration = (std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    cout<<"Calculation time: "<< duration <<'\n';	
+    cout<<"\n Calculation time: "<< duration <<'\n';	
+	return 1;
+
 	}
 
 
@@ -298,45 +291,27 @@ int main()
 {
 
 	int trials;
-	trials = 2;
+	trials = 3;
 
-	 mpz_class h1,h2, h3,t1;
-	 h1 = 561;
-	 h2 = 1;
-	 h3 = 1729;
-	 //1729
+	 mpz_class t1;
+	 int count, res, size, startIndex, j;
+	 j=0;
+	 mpz_class* data = genTestData(j);
+	 size =200;
+	 startIndex = 0;
+	 
+	 for (int i = startIndex; i < size; ++i)
+	 {
+	 	t1 = data[i];
+	  	res =factorize(t1, trials);
+	  	count = count+res;
+	 }
 
-	 mpz_class* data = genTestData(100,20);
-	 //data 4 bra exempel på pollard 
-	 t1 = data[1];
-	 cout << "\nt1 is " << t1;
-	 //cout << "\npollard says " << pollard(h1,h2,h3);
+	cout << "\n Could factorize  " << count << " numbers out of 200 starting on number "<<startIndex <<"whith j = " << 0;
+	 
+	
+	
 
-
-	 //	cout << "is it red alert here?";
-	 factorize(t1, trials);
-	 //factorize(h2, trials);	
-
-	// cout << "test 1 " <<gcd(h1,h2);
-	// cout << "test 2 " <<gcd(h2,h1);
-	// cout << "test 1 " <<pollard(h1,h2,h3);	
-	// cout << "\n t1 result "<< isPrime(t1, 10)<< "\n";
-	// 
-	// for(int i=0; i<100*2;++i){
-	// 	cout << "\n " << data[i];
-	// }
-
-		
 	return (0);
 }
-
-
-
-
-	// mpz_init (integ);
-	// mpz_set_str(integ,"2849585738957348573897639763487638376",10);
- //  	printf ("integ");
- //  	mpz_out_str(stdout,10,integ);
- //  	printf ("\n");
-  	// mpz_clear(integ);
 
