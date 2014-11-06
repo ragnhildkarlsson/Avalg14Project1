aa68fd70a9f4e0fce6fae7ae9d0add437066ebd7
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2014-11-06 15:42:04 +0100

    can generate many batches of smoothpolynoms

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 static const  int smallPrimes[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
 static const int primeLength = 26;
 static const int primeSafety = 10;
-static const int maxB = 500000;
+static const int maxB = 5000000;
 static const double e = 2.718281;
 
 
@@ -178,12 +178,12 @@ int isPrime(mpz_class n, int rep){
 	return 1;
 }
 
-mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
+mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N, int timeOut){
 	// check small primes
 
-	//std::clock_t pClock;
-    //double duration;
-    //pClock = std::clock();
+	std::clock_t pClock;
+    double duration;
+    pClock = std::clock();
 
 	for(int i = 0; i < primeLength; ++i){
 		if((N % smallPrimes[i]) == 0){
@@ -226,9 +226,9 @@ mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 		if(p>1){
 			return p;
 		}
-		//duration = (std::clock() - pClock ) / (double) CLOCKS_PER_SEC;
+	duration = (std::clock() - pClock ) / (double) CLOCKS_PER_SEC;
 		//cerr << "trying...";
-	}while(a!=b);
+	}while(a!=b && duration<timeOut);
 
 
 	
@@ -240,7 +240,7 @@ mpz_class pollard(mpz_class seed, mpz_class add, mpz_class N){
 	return p;
 
 }
- std::vector<mpz_class> genPrimeBase(){
+vector<mpz_class> genPrimeBase(){
 	mpz_class i;
 	i =2;
 	vector<mpz_class> primes;
@@ -334,6 +334,52 @@ mpz_class shanksTonelli(mpz_class n,mpz_class p){
 	}
 }
 
+mpz_class getPrimeFactor(mpz_class N, int trials, int timeOut){
+	
+	gmp_randclass rand (gmp_randinit_default);
+	mpz_class seed, add, factor, temp,error;
+	error = -1;
+	bool primFound = false;
+	factor = N;
+	if(isPrime(factor,10)){
+		return factor;
+	}
+
+	while(!primFound){
+		//cerr << "\n Entering primFound loop with factor: \n " << factor << "\n Trial nr: ";
+		for(int i = 0; i < trials; ++i){
+			seed = rand.get_z_range(factor-1) +1;
+			add = 1; //TODO Hard CODED
+			//cerr << i << " ";
+			//cerr << "\n Seed is \n " << seed;
+			temp = pollard(seed, add, factor,timeOut);
+				if(temp > 0){
+					factor = temp;
+					//cerr << "\nIs the factor " << factor << " considered prime? " << isPrime(factor,10);
+					if(factor > 0 && isPrime(factor,primeSafety)){
+						return factor;
+					}
+					// Compsite number found, try to factor it into a prime
+					if(factor > 0) {
+						break;
+					}
+				}
+			}
+			// Tried trial times and still failed?
+			if(temp < 0){
+				cout << "\n Factorization failed!";
+				//Return -1 to signal failing factorization
+				return error;
+			}
+
+	}
+	cout << "\n Factorization failed!";
+	return error;
+
+	
+}
+
+
 std::vector<mpz_class> factorize(mpz_class N, int trials){
 	
 	vector<mpz_class> factors;	
@@ -357,7 +403,7 @@ std::vector<mpz_class> factorize(mpz_class N, int trials){
 				add = 1; //TODO Hard CODED
 				//cerr << i << " ";
 				//cerr << "\n Seed is \n " << seed;
-				temp = pollard(seed, add, factor);
+				temp = pollard(seed, add, factor, 10);
 					if(temp > 0){
 						factor = temp;
 						//cerr << "\nIs the factor " << factor << " considered prime? " << isPrime(factor,10);
@@ -389,6 +435,112 @@ std::vector<mpz_class> factorize(mpz_class N, int trials){
 		}
 	return factors;
 	}
+
+map<mpz_class, vector<bool> > genSmothPolynoms(mpz_class N, mpz_class sqrtN, vector<mpz_class> & factorBase, vector<mpz_class> & x1List, vector<mpz_class> & x2List, vector<double> & logPList){
+	map<mpz_class, vector<bool> > factoredPolynoms;
+	mpz_class xInPolynom =1;
+	mpz_class firstXInBatch =1;
+	mpz_class p,temp;
+
+	while(factoredPolynoms.size()  < 400){//factorBase.size() ){ // < (factorbase.size + 10) TODO
+	//Create polynoms
+	cerr << "\n Trying to generate more smoth polynoms \n";
+	int nPolynoms = 21474836;
+	vector<mpz_class>  polynoms(nPolynoms);
+	vector<double> polynomsLog(nPolynoms);
+
+	
+	for(int i =1; i<=nPolynoms;++i){
+
+		mpz_class qx = pow(sqrtN+xInPolynom, 2) - N;
+		polynoms.at(i-1) = (qx);
+		polynomsLog.at(i-1) = (log(qx.get_d()));
+		xInPolynom = xInPolynom+1;
+	}
+	cerr <<"\n polynoms created, size is " << polynoms.size();
+	
+	// - SIEVE TIME -
+	//Sieve the polynomsLog
+	//Obs ignoring p==2
+		
+	for(unsigned i = 1; i< factorBase.size(); ++i){
+		
+		p = factorBase.at(i);
+
+		//Add p until reach x-values in this batch 
+		while(x1List.at(i) < firstXInBatch){
+			x1List.at(i) = x1List.at(i) +p;
+		}
+		while(x2List.at(i) < firstXInBatch){
+			x2List.at(i) = x2List.at(i) +p;
+		}
+		
+		//cerr << "\n Entering log sieve with p "<< p; 
+		mpz_class x1Index = x1List.at(i) - firstXInBatch;
+		mpz_class x2Index = x2List.at(i) - firstXInBatch;
+
+		for(unsigned long j = x1Index.get_ui(); j < polynomsLog.size(); j=j+p.get_ui()){
+
+			polynomsLog.at(j) = polynomsLog.at(j) - logPList.at(i);		
+		}
+		for(unsigned long j = x2Index.get_ui(); j < polynomsLog.size(); j=j+p.get_ui()){
+			polynomsLog.at(j) = polynomsLog.at(j) - logPList.at(i);
+		}
+
+	}
+
+	double factorLimit = log(factorBase.back().get_d())+1;
+	vector<mpz_class> factors;
+	
+	cerr << "\n done with log sieving factor limit is " << factorLimit;
+
+
+	// sieve out the smooth polynoms.
+	
+	int count=0;
+	for(unsigned i =0; i<polynomsLog.size();++i){
+		//cerr << "\n polynomLog resten är " << polynomsLog.at(i) << " and index is " << i<<" polynom is "<< polynoms.at(i);
+		if(polynomsLog.at(i)< factorLimit){
+			count++;
+			mpz_class numberToFactor = polynoms.at(i);
+			vector<bool> polynomInFactorBase(factorBase.size(), false);	
+			while(numberToFactor!= 1){
+				temp = getPrimeFactor(numberToFactor,10,20);
+				bool validFactor = false;
+				//check if recived factor is in factorbase				
+				if (temp>0){
+					for(unsigned k = 0; k < factorBase.size(); ++k){
+						if(factorBase.at(k) == temp){ // Factor in factorbase, prepare vector 
+							polynomInFactorBase.at(k) = !polynomInFactorBase.at(k); // flip value
+							numberToFactor= numberToFactor/temp;
+							validFactor = true;
+							break;
+						}
+					}
+				}
+				if(validFactor == false){
+					break;
+				}
+			}
+			if(numberToFactor ==1){
+				factoredPolynoms[polynoms.at(i)] = polynomInFactorBase;
+			}
+		}
+
+	}
+		// Factorization of polynoms complete. Time to gauss
+		cerr << "\n Number of logsmooth polynoms " << count ;		
+		cerr << "\n FactorBase size is " << factorBase.size();
+		cerr << "\n Number of polynoms generated " << polynoms.size();
+		cerr << "\n Number of factored polynoms " << factoredPolynoms.size();
+
+		firstXInBatch = xInPolynom;
+	}
+	return factoredPolynoms;
+
+
+}
+
 
 int quadraticSieve(mpz_class N, vector<mpz_class> & primes){
 	
@@ -425,7 +577,7 @@ int quadraticSieve(mpz_class N, vector<mpz_class> & primes){
 	std::vector<double> logPList(factorBase.size());
 	std::vector<mpz_class> x1List(factorBase.size());
 	std::vector<mpz_class> x2List(factorBase.size());
-	
+
 	mpf_class floatN = N;
 	floatN = sqrt(floatN);
 	floatN = floor(floatN);
@@ -442,101 +594,20 @@ int quadraticSieve(mpz_class N, vector<mpz_class> & primes){
 		x1 = (R1-sqrtN) % p;
 		x2 = (R2 -sqrtN) % p;
 		double pLog = log(p.get_d());		
-		if(x1 <=0){
-			x1 = x1+p;
-		}
-		if(x2 <=0){
-			x2 = x2+p;
-		}
+		// if(x1 <=0){
+		// 	x1 = x1+p;
+		// }
+		// if(x2 <=0){
+		// 	x2 = x2+p;
+		// }
 		logPList.at(i) = pLog;
 		x1List.at(i) = x1;
 		x2List.at(i) = x2;
 	}
 
-
-
+	map<mpz_class, vector<bool> > smoothPolynoms =  genSmothPolynoms(N,sqrtN,factorBase, x1List, x2List,logPList);
+	// - MAIN LOOP - 
 	// Time to enter main loop trying to create enough smooth polynoms!
-	map<mpz_class, vector<bool> > factoredPolynoms;
-	while(factoredPolynoms.size()  < 200 ){ // < (factorbase.size + 10) TODO
-	//Create polynoms
-	int nPolynoms = 21474836;
-	vector<mpz_class>  polynoms(nPolynoms);
-	vector<double> polynomsLog(nPolynoms);
-
-
-	
-	
-	for(int i =1; i<=nPolynoms;++i){
-
-		mpz_class qx = pow(sqrtN+i, 2) - N;
-		polynoms.at(i-1) = (qx);
-		polynomsLog.at(i-1) = (log(qx.get_d()));
-	}
-	cerr <<"\n polynoms created, size is " << polynoms.size();
-	
-	// - SIEVE TIME -
-	//Sieve the polynomsLog
-	//Obs ignoring p==2
-		
-	for(unsigned i = 1; i< factorBase.size(); ++i){
-		
-		p = factorBase.at(i);
-		//cerr << "\n Entering log sieve with p "<< p; 
-		for(unsigned long j = x1List.at(i).get_ui(); j<polynomsLog.size();j=j+p.get_ui()){
-			polynomsLog.at(j-1) = polynomsLog.at(j-1) - logPList.at(i);		
-		}
-		for(unsigned long j = x2List.at(i).get_ui(); j<polynomsLog.size();j=j+p.get_ui()){
-			polynomsLog.at(j-1) = polynomsLog.at(j-1) - logPList.at(i);
-		}
-
-	}
-
-	double factorLimit = log(factorBase.back().get_d())+1;
-	vector<mpz_class> factors;
-	
-	cerr << "\n done with log sieving factor limit is " << factorLimit;
-
-
-	// sieve out the smooth polynoms.
-	
-	int count=0;
-	for(unsigned i =0; i<polynomsLog.size();++i){
-		//cerr << "\n polynomLog resten är " << polynomsLog.at(i) << " and index is " << i<<" polynom is "<< polynoms.at(i);
-		if(polynomsLog.at(i)< factorLimit){
-				count++;
-				factors = factorize(polynoms.at(i),10);
-				vector<bool> polynomInFactorBase(factorBase.size(), false);
-				bool validFactors = false;
-				//cerr << "\n polynom is " << polynoms.at(i) << "which has factors : ";
-				for (unsigned j = 0; j < factors.size(); ++j){
-					//cerr << "\n" << factors.at(j);
-					validFactors = false;
-
-					for(unsigned k = 0; k < factorBase.size(); ++k){
-						if(factorBase.at(k) == factors.at(j)){ // Factor in factorbase, prepare vector 
-							polynomInFactorBase.at(k) = !polynomInFactorBase.at(k); // flip value
-							validFactors = true;
-							break;
-						}
-
-					}
-
-					if(!validFactors){ // we cannot use this poly as it does not factor over the base
-						break;
-					}
-				}
-
-				if(validFactors){ // only if all factors in the poly factors over the base
-					factoredPolynoms[polynoms.at(i)] = polynomInFactorBase;
-				}
-			}
-		}
-		// Factorization of polynoms complete. Time to gauss
-		cerr << "\n Number of logsmooth polynoms " << count ;		
-		cerr << "\n FactorBase size is " << factorBase.size();
-		cerr << "\n Number of polynoms generated " << polynoms.size();
-		cerr << "\n Number of factored polynoms " << factoredPolynoms.size();
-}
 
 	// log p, t, -t (//implement shanks-tonelli)
 
@@ -546,15 +617,19 @@ int quadraticSieve(mpz_class N, vector<mpz_class> & primes){
 }
 
 
-
+void testGetPrimeFactor(){
+	mpz_class res;
+	mpz_class N = 57385973589534549;
+	res = getPrimeFactor(N,10,20);
+	cerr <<"\n found factor "<< res << " in " << N;
+}
 
 
 
 int main() {	
-	int maxInt = std::numeric_limits<int>::max();
-	cerr <<"\n Max int is " << maxInt;
+	
 	// //Gen test data
-	int j =-30;		
+	int j =0;		
 	mpz_class* data = genTestData(j);
 
 	//Gen prime base
@@ -604,7 +679,7 @@ int main() {
 	// mpz_class t1;
 	// t1 = 90283;
 	quadraticSieve(data[1],primes);
-	
+	//testGetPrimeFactor();
 
 	return (0);
 }
